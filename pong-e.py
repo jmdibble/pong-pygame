d0f7c1fcee7ad89daed76229e8ce81df0da6bce1
Josh Dibble <32303700+jmdibble@users.noreply.github.com>
2019-01-26 14:17:47 +0000

    Finally revisited and got it working.
    Need to work on scoring and more specific mechanics. Will also try and make it look prettier.

--- pong-e.py
+++ pong-e.py
@@ -6,46 +6,119 @@ class Pong(object):
         self.screensize = screensize
         self.centerx = int(screensize[0]*0.5)
         self.centery = int(screensize[1]*0.5)
-        self.radius = 5
+        self.radius = 8
         self.rect = pygame.Rect(self.centerx-self.radius,
                                 self.centery-self.radius,
                                 self.radius*2, self.radius*2)
         self.color = (100,100,255)
         self.direction = [1,1]
-        self.speed = 5
-        ## Task: change speed as game progresses to make it harder
+        self.speedx = 2
+        self.speedy = 2
+        ## Task: change speed/radius as game progresses to make it harder
 
         self.hit_edge_left = False
         self.hit_edge_right = False
 
-    def update(self, player_paddle=None, ai_paddle=None):
-        self.centerx += self.direction[0]*self.speed
-        self.centery += self.direction[1]*self.speed
+    def update(self, player_paddle, ai_paddle):
+        self.centerx += self.direction[0]*self.speedx
+        self.centery += self.direction[1]*self.speedy
 
         self.rect.center = (self.centerx, self.centery)
 
         if self.rect.top <= 0:
             self.direction[1] = 1
-        elif self.rect.bottom >= self.screensize[1]-1:
+        elif self.rect.bottom >= self.screensize[1] - 1:
             self.direction[1] = -1
 
-        if self.rect.right >= self.screensize[0]-1:
+        if self.rect.right >= self.screensize[0] - 1:
             self.hit_edge_right = True
         elif self.rect.left <= 0:
             self.hit_edge_left = True
 
-    def render(selfself, screen):
-        pygame.draw.circle
+        ## Task: Change the direction of the pong based on where it hits the paddles
 
+        if self.rect.colliderect(player_paddle.rect):
+            self.direction[0] = -1
+        if self.rect.colliderect(ai_paddle.rect):
+            self.direction[0] = 1
+
+    def render(self, screen):
+        pygame.draw.circle(screen, self.color, self.rect.center, self.radius, 0)
+        pygame.draw.circle(screen, (0,0,0), self.rect.center, self.radius, 1)
+
+
+class AIPaddle(object):
+    def __init__(self, screensize):
+        self.screensize = screensize
+
+        self.centerx = 5
+        self.centery = int(screensize[1]*0.5)
+
+        self.height = 100
+        self.width = 10
+
+        self.rect = pygame.Rect(0, self.centery - int(self.height*0.5), self.width, self.height)
+
+        self.color = (255, 100, 100)
+
+        ## Task: Adjust size of AI paddle as match progresses to make it more difficult
+
+        self.speed = 3
+
+    def update(self, pong):
+        if pong.rect.top < self.rect.top:
+            self.centery -= self.speed
+        elif pong.rect.bottom > self.rect.bottom:
+            self.centery += self.speed
+
+        self.rect.center = (self.centerx, self.centery)
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > self.screensize[1] - 1:
+            self.rect.bottom = self.screensize[1] -1
+
+    def render(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect, 0)
+        pygame.draw.rect(screen, (0,0,0), self.rect, 1)
+
+class PlayerPaddle(object):
+    def __init__(self, screensize):
+        self.screensize = screensize
+
+        self.centerx = screensize[0] - 5
+        self.centery = int(screensize[1]*0.5)
+
+        self.height = 100
+        self.width = 10
+
+        self.rect = pygame.Rect(0, self.centery - int(self.height*0.5), self.width, self.height)
+
+        self.color = (100, 255, 100)
+
+        ## Task: Adjust size of Player paddle as match progresses to make it more difficult
+
+        self.speed = 3
+        self.direction = 0
+
+    def update(self):
+        self.centery += self.direction * self.speed
+
+        self.rect.center = (self.centerx, self.centery)
+
+    def render(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect, 0)
+        pygame.draw.rect(screen, (0,0,0), self.rect, 1)
 
 def main():
     pygame.init()
 
-    screensize = (640,480)
+    screensize = (1040,480)
 
-    screen = pygame.display.set_mode((640,480))
+    screen = pygame.display.set_mode((1040,480))
     clock = pygame.time.Clock()
     pong = Pong(screensize)
+    ai_paddle = AIPaddle(screensize)
+    player_paddle = PlayerPaddle(screensize)
     running = True
 
     while running:
@@ -57,8 +130,21 @@ def main():
             if event.type == QUIT:
                 running = False
 
+            if event.type == KEYDOWN:
+                if event.key == K_UP:
+                    player_paddle.direction = -1
+                elif event.key == K_DOWN:
+                    player_paddle.direction = 1
+            if event.type == KEYUP:
+                if event.key == K_UP and player_paddle.direction == -1:
+                    player_paddle.direction = 0
+                elif event.key == K_DOWN and player_paddle.direction == 1:
+                    player_paddle.direction = 0
+
         ## Object updating phase
-        pong.update()
+        ai_paddle.update(pong)
+        player_paddle.update()
+        pong.update(player_paddle, ai_paddle)
 
         ## Task: make some text on the screen over everything else
         ## Allow restarting of the game
@@ -70,10 +156,15 @@ def main():
             running = False
 
         ## Rendering phase
-        screen.fill((0,0,0))
+        screen.fill((100,100,100))
+
+        ai_paddle.render(screen)
+        player_paddle.render(screen)
+        pong.render(screen)
+
         pygame.display.flip()
 
     pygame.quit()
 
-    
+
 main()
